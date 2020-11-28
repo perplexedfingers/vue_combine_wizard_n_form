0f0f587aa128795045288f899fdb60a0663da571
zhuray <zhuray@noemail.net>
2020-11-28 17:16:59 +0000

    Finish POC
    FossilOrigin-Name: 081056120b1ec1b9adcbde16efd03c1bcd41c41b384d9d05867e07021c32e120

--- TODO.rst
+++
@@ -1,7 +0,0 @@
-TODO
-===
-
-[V] Make a Vue app without nodejs
-[ ] Make a form
-[ ] Make a form that stores data to `store`
-[ ] Make a form that reflects `store`
--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment (state) {
-      state.count++
+const store = new Vuex.Store({})
+
+const ComponentA = {
+  computed: {
+    message: {
+      get () {return store.state.a.message},
+      set (value) {store.commit('a/set', value)}
     }
+  },
+  template: `
+  <div>
+    <br/>
+    Hello from component-a
+    <br/>
+    <input v-model="message"/>
+    <p>{{ this.message }}</p>
+  </div>`,
+  created: function() {
+    store.registerModule(
+    'a',
+      {
+        namespaced: true,
+        state: () => ({
+          message: ''
+        }),
+        mutations: {
+          set (state, value) {
+            state.message = value
+          }
+        }
+      }
+    )
   }
-})
+}
 
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello from Vue!'
+  },
+  components: {
+    'component-a': ComponentA
   },
   store: store
 })
