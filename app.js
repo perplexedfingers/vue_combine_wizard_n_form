e465e91df977299540b8a38e8296e900ba1b2ac7
zhuray <zhuray@noemail.net>
2020-11-28 12:21:36 +0000

    Use Vuex
    FossilOrigin-Name: b68048eb19ff55999a529f716a37b0fc3b6fc3e9f809288eb4b404b5793e3705

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
-  }
+  },
+  store: store
 })
